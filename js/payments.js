37c81bbc378e216f16be7b7b71a662709679c9a8
EdgarMeCa <edgar.ledpa@gmail.com>
2017-07-07 13:45:31 -0500

    Se agrego la funcionalidad de ocultar y mostar divs en la pagina de pagos, ademas de el formulario de prueba, tambien se comento el codigo php de dicha pagina y se cambio la extension a html con esto hay que revertirlo y dejarlo en su forma original

---
+++ js/payments.js
@@ -0,0 +1,21 @@
+//Functions that hides and displays elements by Id
+function esconde_div(hideScreen){
+    var elemento = document.getElementById(hideScreen);
+    elemento.style.display = 'none';
+}
+function visible_div(showScreen){
+    var elemento = document.getElementById(showScreen);
+    elemento.style.display = 'block';
+}
+
+//Function that hides and displays the screens based on the task to perform on the page
+function logicScreens(openScreen){
+    switch(openScreen) {
+        case 'studentPay': 
+            visible_div("studentPay");
+            break;
+        default: 
+            
+    }
+    
+}
